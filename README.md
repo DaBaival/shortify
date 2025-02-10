150e80ff2559c84c206447043d2a8796791c9254
DaBaival <148878832+DaBaival@users.noreply.github.com>
2025-02-10 23:02:12 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# Shortify
+
+A revamped version of the free GitHub Pages link shortener Shortify.
+
+## Adding links
+
+Add a link to the shortener by adding it to `routes.yml`, committing and pushing to main. This will trigger a GitHub workflow to build and deploy the new link automatically.
+
+See [Deploy](#deploy) for more details.
+
+## Dev setup
+
+Clone the repo and cd into the directory. Then follow the steps below to set up a virtual environment and install the dependencies:
+
+```bash
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
+```
+
+> [!IMPORTANT]
+> For local development, you have to include `.html` after the link. For example the link to `/python` would be `/python.html`
+
+## Dev commands
+
+### Build
+
+Run `build.py` while inside the virtual environment.
+
+```bash
+python build.py
+```
+
+### Deploy
+
+Deploying is easy, following the steps below:
+
+1. Add/update the routes in `routes.yml` with the format `name: url` for new links
+2. Add and commit the file to Git
+3. Push to main
+
+GitHub Actions will take care of the rest, and deploy the new shortened links.
+
+> [!NOTE]
+> You can add/edit/delete links quickly by updating the `routes.yml` file in the GitHub UI. Committing to main will redeploy with your changes.
---
+++ build.py
@@ -0,0 +1,74 @@
+import os
+from pathlib import Path
+from typing import NamedTuple
+import shutil
+
+import yaml
+from dotenv import load_dotenv
+
+load_dotenv()
+ROUTE_CONFIG_PATH = Path("routes.yml")
+BUILD_OUTPUT = Path(os.environ.get("BUILD_OUTPUT", "dist"))
+REPO_URL = os.environ.get("REPO_URL", "https://github.com/DaBaival/shortify")
+CNAME = os.environ.get("CNAME")
+
+
+class Link(NamedTuple):
+    name: str
+    url: str
+
+
+def clear_build_output():
+    """Clear the build output directory."""
+    try:
+        for file in BUILD_OUTPUT.iterdir():
+            file.unlink()
+    except FileNotFoundError:
+        pass  # No need to clear an inexistent directory
+
+
+def create_link_page(link: Link):
+    """
+    Create an HTML link page for the given link.
+
+    Relies on parent directory to exist.
+    """
+    with open("template.html", "r") as f:
+        template = f.read()
+        page = template.format(name=link.name, url=link.url)
+
+    with open(BUILD_OUTPUT / f"{link.name}.html", "w") as f:
+        f.write(page)
+
+
+def add_public():
+    """Adds all files in /public to the build output directory."""
+    shutil.copytree("public", BUILD_OUTPUT, dirs_exist_ok=True)
+
+
+def add_index():
+    """Adds index.html to the build output directory."""
+    with open("index.html", "r") as f:
+        template = f.read()
+        page = template.format(addLinkUrl=REPO_URL + "/blob/main/routes.yml")
+
+    with open(BUILD_OUTPUT / "index.html", "w") as f:
+        f.write(page)
+
+
+if __name__ == "__main__":
+    Path(BUILD_OUTPUT).mkdir(parents=True, exist_ok=True)
+    clear_build_output()
+    add_public()
+    add_index()
+
+    if CNAME is not None:
+        with open(BUILD_OUTPUT / "CNAME", "w") as f:
+            f.write(CNAME)
+
+    with open(ROUTE_CONFIG_PATH, "r") as f:
+        data = yaml.safe_load(f)
+
+    for name, url in data.items():
+        link = Link(name=name, url=url)
+        create_link_page(link)
